a57427154ea70b98e18ecc53d12045986cad14fa
Makhaan <makhaan.gupta@enpointe.io>
2024-09-18 14:40:09 +0530

    ot and changes updated

--- cypress/e2e/outlookindia/Storypage.cy.js
+++ cypress/e2e/outlookindia/Storypage.cy.js
@@ -1,8 +1,10 @@
+
     describe('page display on medium size screen',
        {
         viewportHeight: 1000,
         viewportWidth: 1650,
      }, () => {
+        
     it('Storypage', () => {
         cy.visit("https://www.outlookindia.com/national/manipur-violence-prohibitory-orders-imphal-valley-thoubal-student-protests-deadline-demands")
         cy.contains('Manipur Violence:')
@@ -12,7 +14,11 @@
         cy.get(':nth-child(6) > :nth-child(3) > .sb-text > .sb-also-read > .row > .col-5 > .article-img-box > a > .article-img').click()
         cy.contains('Manipur CM Biren Singh On Drone Attacks On Civilians')
         cy.go('back')
+      })
+
+       
         
 })
-
+Cypress.on('uncaught:exception', (err, runnable) => {
+    return false
 })
